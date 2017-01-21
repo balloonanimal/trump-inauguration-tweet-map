674d7d245ff2dbde4d14f68ab61087b671f7b79e
Zachary Rothenberg <zrothenberg1@gmail.com>
2017-01-21 14:36:49 -0500

    tweet harvesting in progress

--- twitterTest.py
+++ twitterTest.py
@@ -1,26 +1,70 @@
-from TwitterSearch import *
+import tweepy
+import sys
+import json
 
-try:
-    tso = TwitterSearchOrder()  # create a TwitterSearchOrder object
-    tso.set_keywords(['#inauguration'])  # let's define all words we would like to have a look for
-    tso.set_language('en')
-    tso.set_include_entities(False) # and don't give us all those entity information
+def cache_tweets():    
 
-    # it's about time to create a TwitterSearch object with our secret tokens
-    ts = TwitterSearch(
-        consumer_key = 'NdjZ21poYPH5CsYWRDrqOm6oZ',
-        consumer_secret = '2FWSpdL7vIwGDmNrEkmLNRGv4GTuoiYTOTKVyHzKuVz1WGOuWY',
-        access_token = '822627811227738117-7vzke77zdqaecczB4TdSon7V1smppNf',
-        access_token_secret = 'Q3S92qdAEiFewtHbT4vL75VJ3G6p0yPKCQHP4yBAt6eUM'
-     )
+    query = "inauguration"
+    max_tweets = 100000000
+    tweets_per_query = 100  # this is the max the API permits
+    file_name = 'tweets.txt' 
 
-    client = MongoClient()
-    db = client['twitter_db']
-    collection = db['twitter_collection']
+    auth = tweepy.AppAuthHandler('NdjZ21poYPH5CsYWRDrqOm6oZ', '2FWSpdL7vIwGDmNrEkmLNRGv4GTuoiYTOTKVyHzKuVz1WGOuWY')
 
-     # this is where the fun actually starts :)
-#     for tweet in ts.search_tweets_iterable(tso):
-         
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+				   wait_on_rate_limit_notify=True)
 
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
-    print(e)
+    if (not api):
+        print("can't get api")
+        sys.exit(1)
+        
+
+    since_id = None
+    max_id = -1
+    count = 0
+    with open(file_name, 'w+') as f:
+        while count < max_tweets: 
+            try:
+                if (max_id <= 0):
+                    if (not since_id):
+                        new_tweets = api.search(q=query, count=tweets_per_query)
+                    else:
+                        new_tweets = api.search(q=query, count=tweets_per_query,
+                                                since_id=sinceId)
+                else:
+                    if (not since_id):
+                        new_tweets = api.search(q=query, count=tweets_per_query,
+                                                max_id=str(max_id - 1))
+                    else:
+                        new_tweets = api.search(q=query, count=tweets_per_query,
+                                                max_id=str(max_id - 1),
+                                                since_id=since_id)
+
+                if not new_tweets:
+                    print("out of tweets")
+                    break
+
+                for tweet in new_tweets:
+                    if tweet.coordinates is not None:
+                        print("found one!")
+                        tweet_map = {'text': tweet.text,
+                                     'coordinates': tweet.coordinates}
+                        json.dump(tweet_map, f)
+                        f.write("\n")
+                    
+                count += tweets_per_query
+                print("{0} tweets downloaded".format(count))
+                max_id = new_tweets[-1].id
+
+
+            except tweepy.TweepError as e:
+                print('error ', e)
+
+def get_tweet_list():
+
+    file = open('tweets.txt', 'r')
+    return [json.load(line) for line in file]
+
+
+cache_tweets()
+
